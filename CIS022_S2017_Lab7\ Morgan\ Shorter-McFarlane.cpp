fc26f4b2b97d5b7208008ef1314ba8057d2218b3
muhCompSciHW <mshorte1@students.solano.edu>
2017-03-09 16:32:39 -0800

    Pseudo code for triangle exercise.

---
+++ CIS022_S2017_Lab7\ Morgan\ Shorter-McFarlane.cpp
@@ -0,0 +1,20 @@
+// Project:
+// Module:
+// Date:
+// Developer:
+// Purpose:
+
+headers
+namespace
+
+function isTriang (a, b, c0
+{
+if (a+b>c) &&....
+{ return 1}
+else return 0
+}
+
+sort value of sides low to high. count number of matching sides. # of matching sides corresponds to an enum value. return value and output.
+
+main{
+}
